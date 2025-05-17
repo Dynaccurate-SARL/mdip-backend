981bf0b5755d4295b319904149430c5ae872d1f4
LuisMSoares <luisedu.soares@dynaccurate.com>
2025-05-17 15:31:31 -0300

    fake json ledger for dev env

--- src/infrastructure/services/confidential_ledger/__init__.py
+++ src/infrastructure/services/confidential_ledger/__init__.py
@@ -1,24 +1,12 @@
 
 import os
-from uuid import uuid4
-from unittest.mock import Mock
 
 from src.infrastructure.services.confidential_ledger.azure_ledger import AzureLedger
 from src.infrastructure.services.confidential_ledger.contract import (
-    LedgerInterface, TransactionInserted)
+    LedgerInterface)
+from src.infrastructure.services.confidential_ledger.fake_json_ledger import FakeJsonLedger
 
 
-def _fake_azure_ledger():
-    def _fake(*args, **kwargs):
-        return TransactionInserted(
-            status='ready',
-            transaction_id=uuid4(),
-            transaction_data={'data': 'fake_data'}
-        )
-    fake = Mock(spec=LedgerInterface)
-    fake.insert_transaction.side_effect = _fake
-    fake.retrieve_transaction.return_value = _fake
-    return fake
 
 
 def ledger_builder(azure_ledger_url: str, 
@@ -27,4 +15,4 @@ def ledger_builder(azure_ledger_url: str,
         return AzureLedger(
             azure_ledger_url, azure_ledger_certificate_path)
     else:
-        return _fake_azure_ledger()
+        return FakeJsonLedger()
