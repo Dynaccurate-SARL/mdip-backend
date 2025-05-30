b2cefa3b1afa31c1ab4f1f751fe736592be7c233
Rafael D'Addio <rafael_daddio@dynaccurate.com>
2025-05-30 16:49:34 -0300

    hotfix: idk parser

--- src/infrastructure/services/pandas_parser/drug/impl/idk.py
+++ src/infrastructure/services/pandas_parser/drug/impl/idk.py
@@ -10,17 +10,19 @@ class DK_Parser(PandasParser):
     def _required_columns(self):
         return ["Drugid", "Navn"]
 
-    def parse(self):
-        self._df["properties"] = self._df.apply(
-            lambda row: row.drop(["Drugid", "Navn"]).dropna().to_dict(), axis=1
-        )
+    def parse(self):        
         try:
             self._df["Registreringsdato"] = self._df["Registreringsdato"].dt.strftime(
                 "%Y-%m-%d"
             )
         except KeyError:
             ...
+        
         self._df = self._df.dropna(thresh=3)
+
+        self._df["properties"] = self._df.apply(
+            lambda row: row.drop(["Drugid", "Navn"]).dropna().to_dict(), axis=1
+        )
 
         # Select relevant columns
         self._df = self._df[["Drugid", "Navn", "properties"]]
@@ -32,3 +34,5 @@ class DK_Parser(PandasParser):
             },
             inplace=True,
         )
+        
+        self._df = self._df.dropna()
