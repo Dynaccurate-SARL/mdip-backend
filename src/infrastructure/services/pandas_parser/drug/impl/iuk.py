8f3c0eecb81d8d70c9e546057a12bc3d262e709b
Rafael D'Addio <rafael_daddio@dynaccurate.com>
2025-07-17 19:14:08 -0300

    feature: uk parser updated

--- src/infrastructure/services/pandas_parser/drug/impl/iuk.py
+++ src/infrastructure/services/pandas_parser/drug/impl/iuk.py
@@ -6,27 +6,23 @@ from src.utils.file import detect_file_encoding
 
 class UK_Parser(PandasParser):
     def _open(self):
-        encoding = detect_file_encoding(self._file)
-        return pd.read_csv(
-            self._file, delimiter=",", encoding=encoding, on_bad_lines="skip", dtype=str
+        return pd.read_json(
+            self._file, dtype=str
         ).where(pd.notnull, None)
 
     def _required_columns(self):
-        return ["VMP_PRODUCT_NAME"]
+        return ["VMP ID", "Name"]
 
     def parse(self):
-        self._df["ID"] = [f"UK_{i + 1}" for i in range(len(self._df))]
-
         self._df["properties"] = self._df.apply(
-            lambda row: row.drop(["ID", "VMP_PRODUCT_NAME"]).dropna().to_dict(), axis=1
+            lambda row: row.drop(["VMP ID", "Name"]).dropna().to_dict(), axis=1
         )
 
-        # Select relevant columns
-        self._df = self._df[["ID", "VMP_PRODUCT_NAME", "properties"]]
+        self._df = self._df[["VMP ID", "Name", "properties"]]
         self._df.rename(
             columns={
-                "ID": "drug_code",
-                "VMP_PRODUCT_NAME": "drug_name",
+                "VMP ID": "drug_code",
+                "Name": "drug_name",
                 "properties": "properties",
             },
             inplace=True,
